3a3a5908ae552f852a61ef3a4aa78f1b6e7496d6
Jericho <fillonjericho@gmail.com>
2021-01-04 22:10:05 +0000

    Create my own assert function to discern whether two arguments are equal.

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  } else if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+/* Test Code */
+assertEqual("Lighthouse Labs", "Bootcamp"); // Non-identical Strings
+assertEqual(1, 1); // Identical Numbers
+assertEqual("Hello", "Hello"); // Identical Strings
+assertEqual(2, 3); // Non-identical Numbers.
