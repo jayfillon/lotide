197487a767d227ec720f9390e2094369f771574e
Jericho <fillonjericho@gmail.com>
2021-01-06 22:39:48 +0000

    Created a function that takes in a sentence (as a string), and return an object with a key of char inputs, and  count of each of the letters frequency.

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// Test Assertion Functions
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// Exercise Function
+// inputString: a String that consist of anything.
+const countLetters = function (inputString) {
+  let results = {};
+
+  for (const char of inputString) {
+    if (char !== ' ') {
+      // If it's found in results, increment++
+      if (results.hasOwnProperty(char)) {
+        results[char] += 1;
+      
+      // Otherwise, create a new key.
+      } else {
+        results[char] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+console.log(countLetters("jericho fillon s2"));
